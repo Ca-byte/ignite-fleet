bf0cf62dec625eb73e94b15b9292cf563ce5f63b
Caroline Vieira <carolinesanvieira@gmail.com>
2024-12-23 22:20:41 +0100

    add styled component

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
+import { ThemeProvider } from 'styled-components/native';
 import { SignIn } from './src/screens/SignIn';
+import theme from './src/theme';
 
 export default function App() {
   return (
+    <ThemeProvider theme={theme}>
     <SignIn />
+  </ThemeProvider>
   );
 }

b2efefdd8a990264a7d25531143d0e9b47be7ddd
Caroline Vieira <carolinesanvieira@gmail.com>
2024-12-30 20:50:29 +0100

    create google login

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,49 @@
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { useState } from 'react';
+import { Alert } from 'react-native';
 import backgroundImg from '../../assets/background.png';
 import { Button } from '../../components/Button';
 import { Container, Slogan, Title } from './styles';
 
+
+
+GoogleSignin.configure({
+  scopes:  ["email", "profile", "openid"],
+  iosClientId: process.env.EXPO_PUBLIC_IOS_CLIENT_ID,
+  webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID,
+})
+
 export function SignIn() {
+  const [isAutenticating, setIsAuthenticanting] = useState(false)
+
+  async function handleGoogleSignIn() {
+    try {
+      setIsAuthenticanting(true)
+
+      const response = await GoogleSignin.signIn();
+      const idToken = response.data?.idToken;  
+      console.log(idToken)
+     
+      if(idToken) {
+
+      } else {
+        Alert.alert('Sign in', "Unable to connect to your google account.")
+        setIsAuthenticanting(false)  
+      }
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Sign in', "Unable to connect to your google account.")
+      setIsAuthenticanting(false)
+    }
+  }
+
   return (
     <Container source={backgroundImg}>
     <Title>Ignite Fleet</Title>
     <Slogan>
       Vehicle usage management
     </Slogan>
-    <Button title='Sign in with Google' />
+    <Button title='Sign in with Google' onPress={handleGoogleSignIn} />
   </Container>
   );
 }
